3fa5d401a6325afbf9d7a27243e3c1c9336fc27a
Emilio Nosthas <nosthas@Emilios-MacBook-Pro-2.local>
2018-03-30 17:35:36 -0600

    Feature Test: No Video

--- test/features/user-visiting-landing-page-test.js
+++ test/features/user-visiting-landing-page-test.js
@@ -1,1 +1,15 @@
 const {assert} = require('chai');
+
+describe('Feature: Landing Page', () => {
+  describe('with no existing videos', () => {
+    it('show no videos', () => {
+      // Setup
+
+      // Exercise
+      browser.url('/');
+
+      // Verify
+      assert.equal(browser.getText('#videos-container'), '');
+    });
+  });
+});

865be131ee6c3b2593a6165574e1456152230bf0
Emilio Nosthas <nosthas@Emilios-MacBook-Pro-2.local>
2018-03-31 09:55:03 -0600

    Add Server Test POST /video creates video document

--- test/routes/video-test.js
+++ test/routes/video-test.js
@@ -3,9 +3,14 @@ const { assert } = require('chai');
 const request = require('supertest');
 const app = require('../../app');
 const { buildVideoObject } = require('../test-utils');
+const { connectDatabaseAndDropData, diconnectDatabase } = require('../database-utils');
 
 // Test Suite
 describe('Server Test: Create Videos', () => {
+
+  beforeEach(connectDatabaseAndDropData);
+  afterEach(diconnectDatabase);
+
   it('POST /videos return 201 code', async () => {
     // Setup
     const videoToCreate = buildVideoObject();
@@ -16,5 +21,19 @@ describe('Server Test: Create Videos', () => {
       .send(videoToCreate);
     // Verify
       assert.equal(response.status, 201);
+  });
+
+  it('POST /videos inserts video document in DB', async () => {
+    // Setup
+    const videoToCreate = buildVideoObject();
+    // Exercise
+    const response = await request(app)
+      .post('/videos')
+      .type('form')
+      .send(videoToCreate);
+    const createdVideo = await Video.findOne({title: videoToCreate.title});
+    // Verify
+    assert.equal(createdVideo.title, videoToCreate.title);
+    assert.equal(createdVideo.description, videoToCreate.description);
   });
 });
